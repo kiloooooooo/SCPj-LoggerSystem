d4c9342f3f3dc9662d4c5de3c95a2d3a88c2b558
KosukeTakahashi <kilotango37@icloud.com>
2019-04-12 22:21:38 +0900

    bugfix

--- python/request-log.py
+++ python/request-log.py
@@ -1,45 +1,24 @@
-######################################
-### MODIFY THIS FILE:              ###
-###   THIS SCRIPT SENDS DUMMY DATA ###
-######################################
-
-
-import json
-import random
-import csv
+import smbus
 import time
+import csv
 from pathlib import Path
 
 
-def dump_log(data: list):
-    targetpath = Path('./logdumps/dump.csv')
+SLAVE_ADDRESS = 0x04
+
+bus = smbus.SMBus(1)
+
+
+def dumplog(data: list):
+    target = Path('./logdumps/dump.csv')
     current = time.asctime()
-    with open(targetpath, 'a', newline='') as f:
+    with open(target, 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow([current] + data)
 
 
-def gen_random(mininum, maximum):
-    val_range = maximum - mininum
-    rand = random.random() * val_range
-    return rand + mininum
-
-
 while True:
     input() # |> ignore
-
-    # Dummy Data
-    log = [
-        gen_random(80, 120),
-        gen_random(0, 150),
-        gen_random(20, 120),
-        gen_random(-4, 4),
-        gen_random(0, 1.2)
-    ]
-
-    dump_log(log)
-
-    message = {
-        'data': log
-    }
-    print(json.dumps(message))
+    log = bus.read_i2c_block_data(SLAVE_ADDRESS, 0, 5)
+    dumplog(log)
+    print(log)
